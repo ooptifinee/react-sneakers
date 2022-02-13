96f5e87afc6e9d3ad010dc8cc3d9c6142265fb02
Dima <->
2022-02-14 00:30:53 +0200

    react-sneakers 1.0

---
+++ src/components/icon.js
@@ -0,0 +1,23 @@
+import logo from './img/header/sneakers.svg'
+import cart from './img/header/cart.svg'
+import favorite from './img/header/favorite.svg'
+import user from './img/header/User.svg'
+import plus from './img/content/plus.svg'
+import plus_non_button from './img/content/plus-non-button.svg'
+import sneak1 from './img/sneakers/1.jpg'
+import sneak2 from './img/sneakers/2.jpg'
+import sneak3 from './img/sneakers/3.jpg'
+import sneak4 from './img/sneakers/4.jpg'
+
+ export default {
+  logo,
+  cart,
+  favorite,
+  user,
+  plus,
+  plus_non_button,
+  sneak1,
+  sneak2,
+  sneak3,
+  sneak4
+}
